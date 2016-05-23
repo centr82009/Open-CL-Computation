665a1809458d74e3e88cbef28bb4baf607b7388b
Роман Мазеев <centr82009@gmail.com>
2016-05-23 23:10:39 +0300

    Initial Commit

---
+++ EVMPROJ2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  EVMPROJ2
+//
+//  Created by Mazeev Roman on 23.05.16.
+//  Copyright © 2016 Mazeev Roman. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
